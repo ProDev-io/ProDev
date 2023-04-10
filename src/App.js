9471dcbc0c754b2c8fbf23296f19fa178add9359
Marucha Chiveli <marucha.webdev@yahoo.com>
2023-04-10 08:11:24 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header bg-black bg-blend-darken">
+        <h1 className='text-4xl text-slate-300 animate-pulse'> React Template Repository with the following packages Pre-installed :</h1>      
+        <ol className='pt-5'>
+           <li>React Router (v5) </li>
+           <li>Tailwind CSS </li>
+            <li> Configuration to use a proxy server.</li>
+            <li>React start script modified to use openssl legacy provider.</li>
+        </ol>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
