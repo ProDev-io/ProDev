6f53b3246869945d2815a3dff721132bae526dde
Marucha <marucha.webdev@yahoo.com>
2023-04-10 09:50:35 +0300

    feat: Render Home Component, global styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
+import Home from './Components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header bg-black bg-blend-darken">
-        <h1 className='text-4xl text-slate-300 animate-pulse'> React Template Repository with the following packages Pre-installed :</h1>      
-        <ol className='pt-5'>
-           <li>React Router (v5) </li>
-           <li>Tailwind CSS </li>
-            <li> Configuration to use a proxy server.</li>
-            <li>React start script modified to use openssl legacy provider.</li>
-        </ol>
-      
-      </header>
+    <div className="App font-main bg-slate-100 min-h-screen text-2xl  m-0 p-0 box-border">
+      <Home />
     </div>
   );
 }
