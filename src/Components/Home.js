c60f8f2568d3cf1ed2f8e929a9e07310fdc01de3
Marucha <marucha.webdev@yahoo.com>
2023-04-11 13:20:09 +0300

    feat: cleanup styling, render footer component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,14 @@
 import About from './About'
 import coding  from '../Images/people-coding.png'
+import Footer from './Footer'
 
 
 function Navigation(){
 	return (
-		<div className="inline-flex p-2 gap-2 cursor-pointer w-full items-center justify-around align-middle space-x-2">
-			<h1 className="text-5xl font-mono">ProDev.io</h1>
-			<h2 className="border-b-4 border-yellow-950">WHAT WE DO</h2>
-			<h2 className="border-b-4 border-yellow-950">TEAM</h2>
-			<h2 className="border-b-4 border-yellow-950">CONTACT US</h2>
+		<div className="inline-flex bg-black sticky text-slate-200 p-4 gap-2 cursor-pointer w-full items-center justify-around align-middle space-x-2 shadow-cyan-50">
+			<h1 className='text-5xl hover:text-yellow-50'>ProDev.io</h1>
+			<h2 className='hover:text-yellow-50'>WHAT WE DO</h2>
+			<h2 className='hover:text-yellow-50'>CONTACT US</h2>
 		</div>
 	)
 }
@@ -18,12 +18,11 @@ function HomeContent(){
 	return (
 		<section className="bg-cover bg-local p-5 w-full h-96 bg-clip-padding" style={{ backgroundImage : `url(${coding})`}}>
 			<div className='p-5 text-slate-600 grid grid-rows-2 gap-6'>
-				<p className='text-5xl p-3 text-white font-semibold'>
+				<p className='text-8xl p-5 text-white font-semibold'>
 					You Imagine it, We Build it!
 				</p>
-
 			<div>
-				<button className='border rounded font-thin bg-blue-300 text-white p-2 hover:bg-slate-200 hover:text-black'>
+				<button className='border-black rounded font-thin bg-black text-white p-2 hover:bg-white hover:text-black'>
 					Tell Us Your Vison
 				</button>
 			</div>
@@ -34,10 +33,11 @@ function HomeContent(){
 
 function Home(){
 	return(
-		<div className="grid gap-5">
+		<div className="grid gap-5 snap-center">
 			<Navigation />
 			<HomeContent />
 			<About />
+			<Footer />
 		</div>
 	)
 }
