1f46bccf8dcd926b9a129fabf7b887e79faa0419
Marucha <marucha.webdev@yahoo.com>
2023-04-11 13:21:08 +0300

    feat: Footer component with contact information and email form

---
+++ src/Components/Footer.js
@@ -0,0 +1,135 @@
+import { useState } from "react";
+import emailjs from 'emailjs-com'
+
+function ContactForm(){
+
+	const [ formData, setFormData ] = useState ({
+		name : '',
+		email : '',
+		subject : '',
+		message : ''
+	});
+
+	const publicKey = "gBYo2VQ45aNmkz7Aq";
+	const privateKey= process.env.REACT_APP_MAIL_JS_SECRET;
+	const service_id = "service_zsy614j";
+	const template_id = "template_0segr99"
+
+	function handleChange(e){
+			e.preventDefault();
+			setFormData({...formData, [e.target.name] : e.target.value });
+	}
+
+	function handleSubmit(e){
+		e.preventDefault();
+
+		emailjs.send(service_id, template_id,{
+			from_subject : formData.subject,
+			from_name : formData.name,
+			from_email : formData.email,
+			message : formData.message
+		}, {
+			private_key : privateKey,
+			public_key : publicKey
+		}).then ((r) => {
+			alert('Emaill sent successfully');
+			console.log(r);
+		}).catch ((e) => {
+			console.log(e)
+		});
+
+		e.target.reset();
+	}
+
+
+	return(
+			<form id='contact_form' className="p-3 flex flex-col gap-3 items-center justify-center align-middle" onSubmit={(e) => handleSubmit(e)}>
+							<label className="block text-left align-middle gap-6 w-full">
+							<span>Your Name</span>
+							<input
+							name="name"
+							type="text"
+							placeholder="Example: John Doe"
+							className="form-input w-full block border rounded-lg text-black"
+							value={formData.name}
+							onChange={(e) => handleChange(e)}/>
+						</label>
+
+						<label className="block text-left align-middle gap-6 w-full">
+							<span>Email</span>
+							<input
+							name="email"
+							type="email"
+							placeholder="Example: visions@prodev.io"
+							className="form-input w-full block border rounded-lg text-black"
+							value={formData.email}
+							onChange={(e) => handleChange(e)}/>
+						</label>
+
+						<label className="block text-left align-middle gap-6 w-full">
+							<span>Subject</span>
+							<input
+							name="subject"
+							type="text"
+							placeholder="Subject"
+							className="form-input block w-full border rounded-lg text-black"
+							value={formData.subject}
+							onChange={(e) => handleChange(e)}/>
+						</label>
+
+						<label className="block text-left align-middle gap-6 w-full">
+						 <span>Message</span>
+						 <textarea
+						 name="message"
+						 id="message"
+						 rows="5"
+						 className="form-input block p-2.5 w-full text-lg text-black bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						 placeholder="Tell us what we shall do for you..."
+						 value={formData.message}
+						 onChange={(e) => handleChange(e)}>
+						 </textarea>
+						</label>
+
+						<input type="submit" placeholder="Send Mail" className="shadow-sm"/>
+		</form>
+
+	)
+}
+
+
+function Footer(){
+	return (
+		<div className="bg-black h-full text-slate-200 p-4 text-left">
+				<h2 className="text-3xl p-2 text-center"> Here's Our Contact Information :</h2>
+				<div className="grid grid-cols-2 gap-6">
+					<ContactForm />
+
+					<div>
+						<p>
+							Questions or Queries :
+						</p>
+
+						<p className="text-yellow-50 flex flex-col">
+							<span className ='underline hover:text-blue-600'>
+								support@prodev.io
+							</span>
+
+							<span className="underline hover:text-blue-600">
+								customercare@prodev.io
+							</span>
+
+							<span className="hover:text-blue-600">
+								Call / Whatsapp : +254 758 925 528 | +254 798 347 396
+							</span>
+						</p>
+					</div>
+				</div>
+
+			<span className="text-sm block text-center align-middle">ProDev Copyright 2023. All Rights Reserved </span>
+		</div>
+	)
+}
+
+export default Footer;
+
+
