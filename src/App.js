0176635387fb08ba061cbf6f2d995f893b7633b8
Marucha <marucha.webdev@yahoo.com>
2023-04-11 13:18:54 +0300

    feat: snap scrolling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App font-main font-medium bg-slate-100 min-h-screen text-2xl m-0 p-0 box-border antialiased leading-loose tracking-wide scroll-smooth">
+    <div className="App font-main font-medium bg-slate-100 min-h-screen text-2xl m-0 p-0 box-border antialiased leading-loose tracking-wide scroll-smooth snap-y">
       <Home />
     </div>
   );
