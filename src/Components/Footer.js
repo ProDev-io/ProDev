72d3861da498981318803e7fb647b4d3851984b4
Marucha <marucha.webdev@yahoo.com>
2023-04-29 18:21:31 +0300

    style: Remove background color for textarea

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -83,7 +83,7 @@ function ContactForm(){
 						 name="message"
 						 id="message"
 						 rows="5"
-						 className="form-input block p-2.5 w-full text-lg text-black bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						 className="form-input block p-2.5 w-full text-lg text-black rounded-lg border"
 						 placeholder="Tell us what we shall do for you..."
 						 value={formData.message}
 						 onChange={(e) => handleChange(e)}>
