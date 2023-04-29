4e4dbd321d735c2cb325f3cfc397057c4cdd8ad6
Marucha <marucha.webdev@yahoo.com>
2023-04-29 18:17:10 +0300

    feat: Define screens for responsiveness, add font to be used in mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,20 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-  ],  theme: {
-    fontFamily : {
-      'main' : ['DM Sans', 'sans-serif']
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        'main' : ['DM Sans', 'sans-serif'],
+        'mobile' : ['IBM Plex Mono', 'sans-serif']
+      },
+      screens: {
+        'mobile' : '375px',
+        'tablet': '640px',
+        'laptop': '1024px',
+        'desktop': '1280px',
+      },
     },
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
