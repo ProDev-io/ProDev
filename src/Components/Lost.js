6d54702e0c9c2ee8e9b29b4d973d73d684cd5fb2
Marucha <marucha.webdev@yahoo.com>
2023-04-30 18:50:02 +0300

    feat: Lost Route Component

---
+++ src/Components/Lost.js
@@ -0,0 +1,23 @@
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
+import lost from '../Images/lost.gif';
+import Footer from './Footer';
+import { Navigation } from './Home';
+
+function Lost(){
+	return (
+		<div className='h-full'>
+				<div className='h-full'>
+					<Navigation />
+					<section className='p-6 m-6 grid grid-col-2 gap-6 items-center justify-center'>
+							<h1 className='text-4xl'> Seems like you're lost </h1>
+							<iframe src={lost} width="100%" height="100%" className="w-full h-96" title="lost" allowFullScreen></iframe>
+					</section>
+					<Footer />
+				</div>
+			</div>
+	)
+};
+
+export default Lost;
