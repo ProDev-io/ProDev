6cebbeabb2e42fb908fe63a3d489645aaef41d09
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-05-23 13:23:22 +0300

    style: Responsive font sizes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Paths from './Components/Routes';
 function App() {
   return (
     <div 
-      className="App font-main bg-slate-100 min-h-screen text-2xl m-0 p-0 box-border antialiased 
-      leading-loose tracking-wide scroll-smooth snap-y w-screen overflow-clip
-      ">
+      className="App desktop:font-main bg-slate-100 min-h-screen desktop:text-2xl m-0 p-0 box-border antialiased 
+      leading-loose tracking-wide scroll-smooth snap-y w-screen overflow-clip mobile:text-md mobile:font-mobile
+      tablet:font-main laptop:font-main"> 
       <Paths />
     </div>
   );
